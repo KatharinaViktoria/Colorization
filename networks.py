b377705c83bc37f2bfce1907de7d178c259cba93
Katharina Viktoria Hoebel <katharinah@bryce.nmr.mgh.harvard.edu>
2018-11-28 11:23:00 -0500

    added patch discriminator

--- networks.py
+++ networks.py
@@ -6,6 +6,31 @@ from torch.optim import lr_scheduler
 
 # Discriminators
 
+
+def init_weights(net, init_type='normal', gain=0.02):
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+            if init_type == 'normal':
+                init.normal_(m.weight.data, 0.0, gain)
+            elif init_type == 'xavier':
+                init.xavier_normal_(m.weight.data, gain=gain)
+            elif init_type == 'kaiming':
+                init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                init.orthogonal_(m.weight.data, gain=gain)
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+            if hasattr(m, 'bias') and m.bias is not None:
+                init.constant_(m.bias.data, 0.0)
+        elif classname.find('BatchNorm2d') != -1:
+            init.normal_(m.weight.data, 1.0, gain)
+            init.constant_(m.bias.data, 0.0)
+
+    print('initialize network with %s' % init_type)
+    net.apply(init_func)
+
+
 def get_norm_layer(norm_type='instance'):
     if norm_type == 'batch':
         norm_layer = functools.partial(nn.BatchNorm2d, affine=True)
@@ -65,3 +90,14 @@ class NLayerDiscriminator(nn.Module):
 
     def forward(self, input):
         return self.model(input)
+
+def create_D(device):
+    norm_layer = get_norm_layer(norm_type='batch')
+    net = NLayerDiscriminator(3, n_layers=3, norm_layer=norm_layer, use_sigmoid=True)
+    net.to(device)
+    init_weights(net, 'normal', 0.02)
+    return net
+
+
+
+
