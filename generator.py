04364cd34082f768be2f93534ceb8ec36ddfdbe7
Katharina Viktoria Hoebel <katharinah@bryce.nmr.mgh.harvard.edu>
2018-11-29 16:10:14 -0500

    cutsom flexible forward function for generator

--- generator.py
+++ generator.py
@@ -5,26 +5,27 @@ import torch.nn.functional as F
 
 # utilities
 import numpy as np
+import random
 
 
 def conv(in_channels, out_channels, kernel_size=3, batch_norm = True):
-	layers = []
-	# convolution with kernel size 4, image size is conserved (stride = 1), bias
-	layers.append(nn.Conv2d(in_channels=in_channels, out_channels=out_channels,kernel_size=kernel_size, stride=1, padding=1, bias=True))
-	if batch_norm:
-		layers.append(nn.BatchNorm2d(out_channels))
-	layers.append(nn.LeakyReLU(0.2, True))
-	return nn.Sequential(*layers)
+    layers = []
+    # convolution with kernel size 4, image size is conserved (stride = 1), bias
+    layers.append(nn.Conv2d(in_channels=in_channels, out_channels=out_channels,kernel_size=kernel_size, stride=1, padding=1, bias=True))
+    if batch_norm:
+        layers.append(nn.BatchNorm2d(out_channels))
+    layers.append(nn.LeakyReLU(0.2, True))
+    return nn.Sequential(*layers)
 
 
 def conv_downsampling(in_channels, out_channels, kernel_size=3, batch_norm=True):
-	layers = []
-	# convolution with kernel size 4, image size is conserved (stride = 1), bias
-	layers.append(nn.Conv2d(in_channels=in_channels, out_channels=out_channels,kernel_size=kernel_size, stride=2, padding=1, bias=True))
-	if batch_norm:
-		layers.append(nn.BatchNorm2d(out_channels))
-	layers.append(nn.LeakyReLU(0.2, True))
-	return nn.Sequential(*layers)
+    layers = []
+    # convolution with kernel size 4, image size is conserved (stride = 1), bias
+    layers.append(nn.Conv2d(in_channels=in_channels, out_channels=out_channels,kernel_size=kernel_size, stride=2, padding=1, bias=True))
+    if batch_norm:
+        layers.append(nn.BatchNorm2d(out_channels))
+    layers.append(nn.LeakyReLU(0.2, True))
+    return nn.Sequential(*layers)
 
 
 def conv_upsampling(in_channels, out_channels, kernel_size=4, batch_norm=True):
@@ -46,7 +47,7 @@ def output_conv(in_channels, kernel_size=1, batch_norm=False):
 
 def fully_connected(max_channels=256, num_classes=10, image_size = 32):
     fully_connected_layers = nn.Sequential(
-        nn.Dropout()
+        nn.Dropout(),
         nn.Linear(image_size/8*image_size/8*max_channels/2,max_channels*4),
         # nn.BatchNorm1d(max_channels*4),
         nn.ReLU(True),
@@ -67,6 +68,7 @@ class unet(nn.Module):
         self.image_size = image_size
         self.classification = classification
         
+        # encoder (shared arm between classification and colorization)
         #1st block (encoder 1)
         # size of output: o = (i-k) +2p +1 -> 32 - 3 +2 + 1 = 32 need kernel size of 3 to keep image size konstant
         self.conv1 = conv(1,max_channels/8,batch_norm=False) #TODO: why no batch norm for first convolution?
@@ -82,6 +84,8 @@ class unet(nn.Module):
 
         # 4rth block (bottleneck) -> attach fully connected layers here
         self.conv4_1 = conv(max_channels/2, max_channels, batch_norm=batch_norm)
+
+        # colorization arm (decoder)
         self.conv4_2 = conv(max_channels, max_channels, batch_norm=batch_norm)
 
         # 5th block
@@ -97,15 +101,27 @@ class unet(nn.Module):
         self.conv7 = conv(max_channels/4, max_channels/8, batch_norm=batch_norm)
         self.conv_out = output_conv(max_channels/8, batch_norm=False)
 
-        # TODO: this will not work this way -> have to flatten the features (compare AlexNet or sth similar, how many features...do we need to further downsample first?????)
-        if classification:
-            self.conv_class = conv(max_channels,max_channels/2, batch_norm=batch_norm)
-            self.classification = fully_connected(max_channels=max_channels, num_classes=num_classes)
+        # classification block
+        self.conv_class = conv(max_channels,max_channels/2, batch_norm=batch_norm)
+        self.classification = fully_connected(max_channels=max_channels, num_classes=num_classes)
 
 
 
 
-    def forward(self, x):
+    def forward(self, x, architecture='random'):
+
+        # architecture defines if the forward pass results in class prediction or image colorization
+        # architecture = random (p=0.5), classification, colorization
+        if architecture=='random':
+            decision = random.random()
+            print(decision)
+            # if random.random()<0.5:
+            if decision<0.5:
+                architecture = 'classification'
+            else:
+                architecture = 'colorization'
+        print(architecture)
+
         horizontal_1 = self.conv1(x)
         out = self.down1(horizontal_1)
 
@@ -116,50 +132,34 @@ class unet(nn.Module):
         out = self.down3(horizontal_3)
 
         features_for_classification = self.conv4_1(out)
-        out = self.conv4_2(features_for_classification)
 
-        # apply upsampling layer
-        out = self.up5(out)
-        # concatenate here and pass the concatenated tensor to next convolution layer
-        out = torch.cat([out, horizontal_3], dim=1)
-        out = self.conv5(out)
+        if architecture=='colorization':
+            out = self.conv4_2(features_for_classification)
 
-        out = self.up6(out)
-        out = torch.cat([out, horizontal_2], dim=1)
-        out = self.conv6(out)
+            # apply upsampling layer
+            out = self.up5(out)
+            # concatenate here and pass the concatenated tensor to next convolution layer
+            out = torch.cat([out, horizontal_3], dim=1)
+            out = self.conv5(out)
 
-        out = self.up7(out)
-        out = torch.cat([out, horizontal_1], dim=1)
-        out = self.conv7(out)
-        col_pred = self.conv_out(out)
+            out = self.up6(out)
+            out = torch.cat([out, horizontal_2], dim=1)
+            out = self.conv6(out)
 
-        if self.classification:
+            out = self.up7(out)
+            out = torch.cat([out, horizontal_1], dim=1)
+            out = self.conv7(out)
+            col_pred = self.conv_out(out)
+
+            return col_pred
+
+        if architecture=='classification':
             out = self.conv_class(features_for_classification)
             out = out.view(out.size(0), self.max_channels/2 * 4 * 4)
             print('flattened: ', out.size())
             class_pred = self.classification(out)
+            return class_pred
 
-        if self.classification:
-            return [col_pred, class_pred]
-        else:
-            return col_pred
-
-
-model = unet(classification=True)
-model.cuda()
-print(model)
-
-example = np.ones([1,1,32,32]).astype(float)
-example = torch.from_numpy(example)
-example = example.type(torch.cuda.FloatTensor)
-
-print(example.type())
-print(example.size())
-col_pred, class_pred = model.forward(example)
-# col_pred = model.forward(example)
-print(col_pred.size())
-print(class_pred)
-
-
+        
 
 
--- generator_master.py
+++ generator_master.py
@@ -3,71 +3,52 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# go with the classical U-Net architecture as described by Ronneberg et al
+# utilities
+import numpy as np
+import random
 
-def conv_layer(in_channels, out_channels, kernel_size = 4, stride=2, padding=1, batch_norm=True, init_zero_weights=False):
-	"""Creates a convolutional layer, with optional batch normalization.
-	"""
-	layers = []
-	conv_layer = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
-	# conv_layer = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=True)
-	if init_zero_weights:
-		conv_layer.weight.data = torch.randn(out_channels, in_channels, kernel_size, kernel_size) * 0.001
-	layers.append(conv_layer)
-
-	if batch_norm:
-		layers.append(nn.BatchNorm2d(out_channels))
-	return nn.Sequential(*layers)
-
-
-def conv_encoder(in_channels, out_channels):
-	return nn.Sequential(
-		nn.Conv2d(in_channels, out_channels, 3, padding=1),
-		nn.ReLU(inplace=True),
-		nn.Conv2d(out_channels, out_channels, 3, padding=1),
-		nn.ReLU(inplace=True)
-	) 
 
 def conv(in_channels, out_channels, kernel_size=3, batch_norm = True):
-	layers = []
-	# convolution with kernel size 4, image size is conserved (stride = 1), bias
-	layers.append(nn.Conv2d(in_channels=in_channels, out_channels=out_channels,kernel_size=kernel_size, stride=1, padding=1, bias=True))
-	if batch_norm:
-		layers.append(nn.BatchNorm2d(out_channels))
-	layers.append(nn.LeakyReLU(0.2, True))
-	return nn.Sequential(*layers)
+    layers = []
+    # convolution with kernel size 4, image size is conserved (stride = 1), bias
+    layers.append(nn.Conv2d(in_channels=in_channels, out_channels=out_channels,kernel_size=kernel_size, stride=1, padding=1, bias=True))
+    if batch_norm:
+        layers.append(nn.BatchNorm2d(out_channels))
+    layers.append(nn.LeakyReLU(0.2, True))
+    return nn.Sequential(*layers)
 
 
 def conv_downsampling(in_channels, out_channels, kernel_size=3, batch_norm=True):
-	layers = []
-	# convolution with kernel size 4, image size is conserved (stride = 1), bias
-	layers.append(nn.Conv2d(in_channels=in_channels, out_channels=out_channels,kernel_size=kernel_size, stride=2, padding=1, bias=True))
-	if batch_norm:
-		layers.append(nn.BatchNorm2d(out_channels))
-	layers.append(nn.LeakyReLU(0.2, True))
-	return nn.Sequential(*layers)
+    layers = []
+    # convolution with kernel size 4, image size is conserved (stride = 1), bias
+    layers.append(nn.Conv2d(in_channels=in_channels, out_channels=out_channels,kernel_size=kernel_size, stride=2, padding=1, bias=True))
+    if batch_norm:
+        layers.append(nn.BatchNorm2d(out_channels))
+    layers.append(nn.LeakyReLU(0.2, True))
+    return nn.Sequential(*layers)
 
 
-# upsamling layer (upsamling and reduction of number of channels by factor 2)
-def conv_upsampling(in_channels, out_channels=in_channels/2, kernel_size=4, batch_norm=True):
-	layers = []
-	layers.append(nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=2, padding=1, bias=True))
-	if batch_norm:
-		layers.append(nn.BatchNorm2d(out_channels))
-	layers.append(nn.LeakyReLU(0.2, True))
-	return nn.Sequential(*layers)
+def conv_upsampling(in_channels, out_channels, kernel_size=4, batch_norm=True):
+    layers = []
+    layers.append(nn.ConvTranspose2d(in_channels=in_channels, out_channels=in_channels/2, kernel_size=kernel_size, stride=2, padding=1, bias=True))
+    if batch_norm:
+        layers.append(nn.BatchNorm2d(out_channels))
+    layers.append(nn.LeakyReLU(0.2, True))
+    return nn.Sequential(*layers)
 
 def output_conv(in_channels, kernel_size=1, batch_norm=False):
-	layers = []
-	layers.append(nn.Conv2d(in_channels=in_channels, out_channels=2, kernel_size=kernel_size, stride=1, bias=False))
+    layers = []
+    layers.append(nn.Conv2d(in_channels=in_channels, out_channels=2, kernel_size=kernel_size, stride=1, bias=False))
     if batch_norm:
-		layers.append(nn.BatchNorm2d(out_channels))
-	layers.append(nn.Tanh())
-	return nn.Sequential(*layers)
+        layers.append(nn.BatchNorm2d(out_channels))
+    layers.append(nn.Tanh())
+    return nn.Sequential(*layers)
 
-def fully_connected(max_channels=256, num_classes=10):
+
+def fully_connected(max_channels=256, num_classes=10, image_size = 32):
     fully_connected_layers = nn.Sequential(
-        nn.Linear(4*4*max_channels/2,max_channels*4),
+        nn.Dropout(),
+        nn.Linear(image_size/8*image_size/8*max_channels/2,max_channels*4),
         # nn.BatchNorm1d(max_channels*4),
         nn.ReLU(True),
         nn.Dropout(),
@@ -80,11 +61,14 @@ def fully_connected(max_channels=256, num_classes=10):
 
 # my code
 class unet(nn.Module):
-    def __init__(self, max_channels = 256, batch_norm=True, classification=True, num_classes=10):
+    def __init__(self, max_channels = 256, batch_norm=True, classification=True, num_classes=10, image_size = 32):
         super(unet,self).__init__()
 
-        self.max_channels=max_channels
+        self.max_channels = max_channels
+        self.image_size = image_size
+        self.classification = classification
         
+        # encoder (shared arm between classification and colorization)
         #1st block (encoder 1)
         # size of output: o = (i-k) +2p +1 -> 32 - 3 +2 + 1 = 32 need kernel size of 3 to keep image size konstant
         self.conv1 = conv(1,max_channels/8,batch_norm=False) #TODO: why no batch norm for first convolution?
@@ -100,6 +84,8 @@ class unet(nn.Module):
 
         # 4rth block (bottleneck) -> attach fully connected layers here
         self.conv4_1 = conv(max_channels/2, max_channels, batch_norm=batch_norm)
+
+        # colorization arm (decoder)
         self.conv4_2 = conv(max_channels, max_channels, batch_norm=batch_norm)
 
         # 5th block
@@ -115,15 +101,27 @@ class unet(nn.Module):
         self.conv7 = conv(max_channels/4, max_channels/8, batch_norm=batch_norm)
         self.conv_out = output_conv(max_channels/8, batch_norm=False)
 
-        # TODO: this will not work this way -> have to flatten the features (compare AlexNet or sth similar, how many features...do we need to further downsample first?????)
-        if classification:
-            self.conv_class = conv(max_channels,max_channels/2, batch_norm=batch_norm)
-            self.classification = fully_connected(max_channels=max_channels, num_classes=num_classes)
+        # classification block
+        self.conv_class = conv(max_channels,max_channels/2, batch_norm=batch_norm)
+        self.classification = fully_connected(max_channels=max_channels, num_classes=num_classes)
 
 
 
 
-    def forward(self, x, classification = True):
+    def forward(self, x, architecture='random'):
+
+        # architecture defines if the forward pass results in class prediction or image colorization
+        # architecture = random (p=0.5), classification, colorization
+        if architecture=='random':
+            decision = random.random()
+            print(decision)
+            # if random.random()<0.5:
+            if decision<0.5:
+                architecture = 'classification'
+            else:
+                architecture = 'colorization'
+        print(architecture)
+
         horizontal_1 = self.conv1(x)
         out = self.down1(horizontal_1)
 
@@ -134,36 +132,50 @@ class unet(nn.Module):
         out = self.down3(horizontal_3)
 
         features_for_classification = self.conv4_1(out)
-        out = self.conv4_2(features_for_classification)
 
-        # apply upsampling layer
-        out = self.up5(out)
-        # concatenate here and pass the concatenated tensor to next convolution layer
-        out = torch.cat([out, horizontal_3], dim=1)
-        out = self.conv5(out)
+        if architecture=='colorization':
+            out = self.conv4_2(features_for_classification)
 
-        out = self.up6(out)
-        out = torch.cat([out, horizontal_2], dim=1)
-        out = self.conv6(out)
+            # apply upsampling layer
+            out = self.up5(out)
+            # concatenate here and pass the concatenated tensor to next convolution layer
+            out = torch.cat([out, horizontal_3], dim=1)
+            out = self.conv5(out)
 
-        out = self.up7(out)
-        out = torch.cat([out, horizontal_1], dim=1)
-        out = self.conv7(out)
-        col_pred = self.conv_out(out)
+            out = self.up6(out)
+            out = torch.cat([out, horizontal_2], dim=1)
+            out = self.conv6(out)
 
-        if classification:
+            out = self.up7(out)
+            out = torch.cat([out, horizontal_1], dim=1)
+            out = self.conv7(out)
+            col_pred = self.conv_out(out)
+
+            return col_pred
+
+        if architecture=='classification':
             out = self.conv_class(features_for_classification)
             out = out.view(out.size(0), self.max_channels/2 * 4 * 4)
             print('flattened: ', out.size())
             class_pred = self.classification(out)
+            return class_pred
 
-        if classification:
-            return [col_pred, class_pred]
-        else:
-            return col_pred
+        
+
+model = unet(classification=True)
+model.cuda()
+print(model)
+
+example = np.ones([1,1,32,32]).astype(float)
+example = torch.from_numpy(example)
+example = example.type(torch.cuda.FloatTensor)
+
+print(example.type())
+print(example.size())
+pred = model.forward(example)
+print(pred.size())
+# print(pred)
 
 
 
-	
 
-		
